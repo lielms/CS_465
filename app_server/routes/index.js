4d5da658e58ac70d0ed6a941b59b16a00b40ca84
lielms <lielmgerby@gmail.com>
2025-05-25 13:08:40 -0700

    Module 3 Completed Baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const ctrlMain = require('../controllers/main');
+const ctrlTravel = require('../controllers/travel');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
+/* GET travel page */
+router.get('/travel', ctrlTravel.travel);
 
 module.exports = router;
